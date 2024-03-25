a1c96b2000f2bf920145d2421e9202858bc65456
Dishant Panchal <dishant.panchal@techavidus.com>
2024-03-25 14:21:11 +0530

    added counter increase and decrease logic

--- vite-react/src/App.jsx
+++ vite-react/src/App.jsx
@@ -1,6 +1,22 @@
-function App() {
+import { useState } from "react";
+
+
+function App() {  
+  const [counter, setCounter] = useState(0);  
+  const addValue = () =>{    
+    setCounter(counter + 1);
+  }
+  const decreaseValue = () =>{    
+    if(counter <=0) setCounter(0);
+    else setCounter(counter - 1);
+  }
   return (
-    <h1>Learning react with vite</h1>
+    <>
+      <h1>Learning react with vite</h1>
+      <h3>Counter value: {counter}</h3>
+      <button onClick={addValue}>Increase Count</button> <br />
+      <button onClick={decreaseValue}>Decrease Count</button>
+    </>
   )
 }
 
